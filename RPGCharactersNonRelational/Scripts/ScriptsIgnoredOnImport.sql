f916dc5c34bf6411d0f339d9a2b6ca02bd0c88d5
Alexandre Homem <alexman657@gmail.com>
2024-06-03 11:19:44 +0100

    03-06-2024 11:19

---
+++ RPGCharactersNonRelational/Scripts/ScriptsIgnoredOnImport.sql
@@ -0,0 +1,130 @@
+﻿
+DECLARE @Name as NVARCHAR(50)
+DECLARE @Race as NVARCHAR(50)
+DECLARE @Class as NVARCHAR(50)
+DECLARE @Background as NVARCHAR(50)
+DECLARE @Height as FLOAT
+DECLARE @Weight as INT
+DECLARE @Homeland as NVARCHAR(50)
+DECLARE @Age as INT
+DECLARE @Mentality_Alignment as NVARCHAR(50)
+DECLARE @Honor_Alignment as NVARCHAR(50)
+DECLARE @Vitality as INT
+DECLARE @Concentration as INT
+DECLARE @Endurance as INT
+DECLARE @Strength as INT
+DECLARE @Dexterity as INT
+DECLARE @Resistance as INT
+DECLARE @Intelligence as INT
+DECLARE @Faith as INT
+DECLARE @Arcane as INT
+DECLARE @Stash as TEXT
+DECLARE @Main_Weapon as NVARCHAR(50)
+DECLARE @Secondary_Weapon AS NVARCHAR(50)
+DECLARE @Third_Weapon AS NVARCHAR(50)
+DECLARE @Armor AS NVARCHAR(50)
+DECLARE @Main_Skills AS TEXT
+DECLARE @Secondary_Skills AS TEXT
+DECLARE @Support_Skills AS TEXT
+DECLARE @Novice_Spells AS TEXT
+DECLARE @Apprentice_Spells AS TEXT
+DECLARE @Journeyman_Spells AS TEXT
+DECLARE @Expert_Spells AS TEXT
+DECLARE @Master_Spells AS TEXT
+DECLARE @Family AS TEXT
+DECLARE @Friends AS TEXT
+DECLARE @Rivals AS TEXT
+DECLARE @Enemies AS TEXT
+DECLARE @Backstory AS NVARCHAR(MAX)
+DECLARE @IMG AS NVARCHAR(MAX) 
+
+
+
+  @Name  = 
+  @Race  = 
+  @Class  = 
+  @Background  = 
+  @Height  = 
+  @Weight  = 
+  @Homeland  = 
+  @Age  = 
+  @Mentality_Alignment  = 
+  @Honor_Alignment  = 
+  @Vitality  = 
+  @Concentration  = 
+  @Endurance  = 
+  @Strength  = 
+  @Dexterity  = 
+  @Resistance  = 
+  @Intelligence  = 
+  @Faith  = 
+  @Arcane  = 
+  @Stash  = 
+  @Main_Weapon  = 
+  @Secondary_Weapon  = 
+  @Third_Weapon  = 
+  @Armor  = 
+  @Main_Skills  = 
+  @Secondary_Skills  = 
+  @Support_Skills  = 
+  @Novice_Spells  = 
+  @Apprentice_Spells  = 
+  @Journeyman_Spells  = 
+  @Expert_Spells  = 
+  @Master_Spells  = 
+  @Family  = 
+  @Friends  = 
+  @Rivals  = 
+  @Enemies  = 
+  @Backstory  = 
+  @IMG  = 
+
+GO
+
+--Syntax Error: Incorrect syntax near @Name.
+--DECLARE @IMG AS NVARCHAR(MAX) 
+--
+--
+--
+--  @Name  = 
+--  @Race  = 
+--  @Class  = 
+--  @Background  = 
+--  @Height  = 
+--  @Weight  = 
+--  @Homeland  = 
+--  @Age  = 
+--  @Mentality_Alignment  = 
+--  @Honor_Alignment  = 
+--  @Vitality  = 
+--  @Concentration  = 
+--  @Endurance  = 
+--  @Strength  = 
+--  @Dexterity  = 
+--  @Resistance  = 
+--  @Intelligence  = 
+--  @Faith  = 
+--  @Arcane  = 
+--  @Stash  = 
+--  @Main_Weapon  = 
+--  @Secondary_Weapon  = 
+--  @Third_Weapon  = 
+--  @Armor  = 
+--  @Main_Skills  = 
+--  @Secondary_Skills  = 
+--  @Support_Skills  = 
+--  @Novice_Spells  = 
+--  @Apprentice_Spells  = 
+--  @Journeyman_Spells  = 
+--  @Expert_Spells  = 
+--  @Master_Spells  = 
+--  @Family  = 
+--  @Friends  = 
+--  @Rivals  = 
+--  @Enemies  = 
+--  @Backstory  = 
+--  @IMG  =
+
+
+
+GO
